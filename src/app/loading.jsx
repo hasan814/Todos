f9706fca77dcdfcdb5dc53e04c78ccc1aa35698d
hasan814 <h.mousavi910@gmail.com>
2024-06-07 21:04:50 +0330

    Hello and Start

---
+++ src/app/loading.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { RotatingLines } from "react-loader-spinner";
+
+const Loading = () => {
+  return (
+    <RotatingLines
+      visible={true}
+      height="96"
+      width="96"
+      color="grey"
+      strokeWidth="5"
+      animationDuration="0.75"
+      ariaLabel="rotating-lines-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+  );
+};
+
+export default Loading;
