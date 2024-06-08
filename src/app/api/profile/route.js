d41d764bbad56166a67a122350d6a1bb254b4be1
hasan814 <h.mousavi910@gmail.com>
2024-06-08 22:01:05 +0330

    Created Profile GET API

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -45,3 +45,42 @@ export async function POST(req) {
     );
   }
 }
+export async function GET(req) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(req);
+    if (!session)
+      return NextResponse.json(
+        { error: "Please log in to your account" },
+        { status: 401 }
+      );
+
+    const email = session.user.email;
+    const user = await User.findOne({ email });
+    if (!user)
+      return NextResponse.json(
+        { error: "User doesn't exist!" },
+        { status: 404 }
+      );
+
+    const { password } = await req.json();
+    const isValid = await verifyPassword(password, user.password);
+    if (isValid)
+      return NextResponse.json(
+        { error: "message is incorect" },
+        { status: 422 }
+      );
+
+    return NextResponse.json(
+      { data: { name: user.name, lastName: user.lastName, email: user.email } },
+      { status: "success" }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "An error has occurred in the DB" },
+      { status: 500 }
+    );
+  }
+}
